cdb6e1a0068c41de5c58ceeb615b0b96e569d3fb
matthewshan <matthewshan99@gmail.com>
2020-02-05 11:13:34 -0500

    Testing API Call from Heroku

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from bot import Bot
+import requests
 
 bot = Bot()
+print(requests.get('https://custardquotesapi.azurewebsites.net/quotes', headers={'ApiKey': bot.get_token('API_KEY')}).json())
 bot.run(bot.get_token('DISCORD_TOKEN'))
