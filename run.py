51713bf817ff8e5a278a2422fe7651b9e06d1d45
Matthew Shan <matthewshan99@gmail.com>
2020-02-06 14:56:06 -0500

    Removed broken request

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from bot import Bot
 import requests
 
 bot = Bot()
-print(requests.get('https://custardquotesapi.azurewebsites.net/quotes', headers={'ApiKey': bot.get_token('API_KEY')}).json())
+# print(requests.get('https://custardquotesapi.azurewebsites.net/quotes', headers={'ApiKey': bot.get_token('API_KEY')}).json())
 bot.run(bot.get_token('DISCORD_TOKEN'))
