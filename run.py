e49747e678edb5b147ebc80e61ea47771a42a56b
Matthew Shan <matthewshan99@gmail.com>
2020-02-08 22:44:51 -0500

    heroku plz work

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from bot import Bot
 import requests
 
 bot = Bot()
-# print(requests.get('https://custardquotesapi.azurewebsites.net/quotes', headers={'ApiKey': bot.get_token('API_KEY')}).json())
-bot.run(bot.get_token('DISCORD_TOKEN'))
+bot.run(os.environ['DISCORD_TOKEN'])
